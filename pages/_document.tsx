e6da5011af043bb6e58642432245c8e1cb9fed6b
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2023-01-08 03:45:51 -0800

    Metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* SEO META TAGS */}
+        <meta name="description" content="Brenden Smith | Software Engineer" />
+        <meta name="og:title" content="Brenden Smith | Software Engineer" />
+        <meta
+          name="og:description"
+          content="Brenden Smith | Software Engineer"
+        />
+        <meta
+          name="og:image"
+          content="https://avatars.githubusercontent.com/u/61305154?v=4"
+        />
+        <meta name="og:url" content="https://brenden-smith.com" />
+      </head>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
