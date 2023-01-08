030e77595b91baceff00558cc60ccdf21aa98785
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2023-01-08 03:45:57 -0800

    Remove comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* SEO META TAGS */}
         <meta name="description" content="Brenden Smith | Software Engineer" />
         <meta name="og:title" content="Brenden Smith | Software Engineer" />
         <meta
