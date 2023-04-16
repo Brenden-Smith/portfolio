2cf85c6785f501dadb90e8d8fadab343d3dbf55c
Brenden Smith <git@brenden-smith.com>
2023-04-16 04:46:05 -0700

    Linting

--- pages/projects/foodood.tsx
+++ pages/projects/foodood.tsx
@@ -54,7 +54,7 @@ export default function Page() {
               FooDood is a mobile application that combines the concept of
               Tinder-swiping with selecting something to eat. Science says that
               it is easier to make a decision when presented with simple options
-              such as "yes" or "no" rather than a large list of options. FooDood
+              such as {"\""}yes{"\""} or {"\""}no{"\""} rather than a large list of options. FooDood
               will present users with a list of food options in their area and
               they will be able to swipe left or right on the options to decide
               what to eat.
